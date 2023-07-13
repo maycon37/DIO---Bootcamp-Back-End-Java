acc5cbad5b62045df6a2cb33b26514bc0bf464ae
Maycon Rodrigues <93656419+maycon37@users.noreply.github.com>
2023-07-13 10:01:03 -0300

    Add files via upload

---
+++ DIO/Projeto 1/src/EX1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class EX1 {
+
+    public static void main(String[] args) {
+
+
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("APROVADO Minerva");
+        else 
+            System.out.println("APROVADO");
+        
+    System.out.println("Digite a média do aluno: ");
+
+    }
+
+}
---
+++ DIO/Projeto 1/src/MinhaClasse.java
@@ -0,0 +1,18 @@
+public class MinhaClasse {
+    public static void main (String[] args) {
+        
+        System.out.print("Olá mundo!");
+
+        String primeiroNome = "Maycon";
+        String segundoNome = "Rodrigues";
+
+        String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    {
+    public static String nomeCompleto(String primeiroNome;String segundoNome) 
+    
+}
+}
